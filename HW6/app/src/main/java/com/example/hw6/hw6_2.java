f97d8eaf6ebf524b745fff4d65ba1c40c09c48cc
MINSU <ancx1234@naver.com>
2022-04-13 13:18:49 +0900

    전문-6주차과제
    0413

--- HW6/app/src/main/java/com/example/hw6/hw6_2.java
+++ HW6/app/src/main/java/com/example/hw6/hw6_2.java
@@ -3,6 +3,12 @@ package com.example.hw6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class hw6_2 extends AppCompatActivity {
 
@@ -10,5 +16,39 @@ public class hw6_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw62);
+
+        EditText edtUrl = findViewById(R.id.edtUrl);
+        Button btnGo = findViewById(R.id.btnGo);
+        Button btnBack = findViewById(R.id.btnBack);
+        WebView web = findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.loadUrl(edtUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.goBack();
+            }
+        });
+
+    }
+
+    class CookWebViewClient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
     }
 }
--- HW6/app/src/main/java/com/example/hw6/hw6_3.java
+++ HW6/app/src/main/java/com/example/hw6/hw6_3.java
@@ -2,13 +2,36 @@ package com.example.hw6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class hw6_3 extends AppCompatActivity {
+@SuppressWarnings("deprecation")
+public class hw6_3 extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw63);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabDog = tabHost.newTabSpec("dog").setIndicator("강아지");
+        tabDog.setContent(R.id.dog);
+        tabHost.addTab(tabDog);
+
+        TabHost.TabSpec tabCat = tabHost.newTabSpec("cat").setIndicator("고양이");
+        tabCat.setContent(R.id.cat);
+        tabHost.addTab(tabCat);
+
+        TabHost.TabSpec tabRabbit = tabHost.newTabSpec("rab").setIndicator("토끼");
+        tabRabbit.setContent(R.id.rabbit);
+        tabHost.addTab(tabRabbit);
+
+        TabHost.TabSpec tabHorse = tabHost.newTabSpec("lion").setIndicator("말");
+        tabHorse.setContent(R.id.horse);
+        tabHost.addTab(tabHorse);
+
+        tabHost.setCurrentTab(0);
     }
 }
